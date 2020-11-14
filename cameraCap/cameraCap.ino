17298d97498432862b1cfa70ab554dedc840a542
pommland <pomm-land@hotmail.co.th>
2020-11-14 16:16:39 +0700

    create camera control

---
+++ cameraCap/cameraCap.ino
@@ -0,0 +1,39 @@
+String incomingByte ;
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+void loop() {
+
+  if (Serial.available() > 0) {
+
+  incomingByte = Serial.readStringUntil('\n');
+
+    if (incomingByte == "on") {
+
+      digitalWrite(LED_BUILTIN, HIGH);
+
+      Serial.write("Led on");
+
+    }
+
+    else if (incomingByte == "off") {
+
+      digitalWrite(LED_BUILTIN, LOW);
+
+      Serial.write("Led off");
+
+    }
+
+    else{
+
+     Serial.write("invald input");
+
+    }
+
+  }
+
+}
